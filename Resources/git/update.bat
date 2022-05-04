178b0c1636fedce8db25ae9aba970ea95bbf551d
Penn Oser <r1ft4469@users.noreply.github.com>
2022-05-04 12:45:39 -0500

    Update update.bat

--- Resources/git/update.bat
+++ Resources/git/update.bat
@@ -4,7 +4,7 @@ git\cmd\git.exe clone --recursive https://github.com/kobrakon/SPO_DEV.git update
 xcopy /Y /E update\\. .
 del .\BepInEx\plugins\r1ft.DynamicTimeCyle.dll
 del .\BepInEx\plugins\r1ft.Headlamps.dll
-xcopy /Y /E .\user\mods\r1ft-DynamicTimeCycle\r1ft.DynamicTimeCyle.dll .\BepInEx\plugins\r1ft.DynamicTimeCyle.dll
-xcopy /Y /E .\user\mods\.SPO\mods\Headlamps\r1ft.Headlamps.dll .\BepInEx\plugins\r1ft.Headlamps.dll
+xcopy /Y /E .\user\mods\r1ft-DynamicTimeCycle\r1ft.DynamicTimeCyle.dll .\BepInEx\plugins\r1ft.DynamicTimeCyle.dll*
+xcopy /Y /E .\user\mods\.SPO\mods\Headlamps\r1ft.Headlamps.dll .\BepInEx\plugins\r1ft.Headlamps.dll*
 rmdir /Q /S update
 Server.exe
