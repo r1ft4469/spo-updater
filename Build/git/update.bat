538a93a424c7aa2c5ac1d6ac2d6f2a69cbda6257
Penn Oser <r1ft4469@users.noreply.github.com>
2022-05-14 18:45:41 -0500

    updaqte git

---
+++ Build/git/update.bat
@@ -0,0 +1,46 @@
+@echo off
+taskkill /F /IM Server.exe
+taskkill /F /IM Aki.Launcher.exe
+del git\release.json
+move git\release.new.json git\release.json
+IF EXIST "git\git.exe" (
+    git\git.exe -y -gm2 -InstallPath="git"
+    del git\git.exe
+    del git\git.7z.001
+)
+rmdir /Q /S user\mods
+mkdir user\mods
+move .\BepInEx\plugins\aki-bundles.dll .\BepInEx\
+move .\BepInEx\plugins\aki-core.dll .\BepInEx\
+move .\BepInEx\plugins\aki-custom.dll .\BepInEx\
+move .\BepInEx\plugins\aki-debugging.dll .\BepInEx\
+move .\BepInEx\plugins\aki-secret.dll .\BepInEx\
+move .\BepInEx\plugins\aki-singleplayer.dll .\BepInEx\
+move .\BepInEx\plugins\ConfigurationManager.dll .\BepInEx\
+del /Q BepInEx\plugins\*
+move .\BepInEx\aki-bundles.dll .\BepInEx\plugins\
+move .\BepInEx\aki-core.dll .\BepInEx\plugins\
+move .\BepInEx\aki-custom.dll .\BepInEx\plugins\
+move .\BepInEx\aki-debugging.dll .\BepInEx\plugins\
+move .\BepInEx\aki-secret.dll .\BepInEx\plugins\
+move .\BepInEx\aki-singleplayer.dll .\BepInEx\plugins\
+move .\BepInEx\ConfigurationManager.dll .\BepInEx\plugins\
+git\cmd\git.exe clone --recursive https://github.com/kobrakon/SPO_DEV.git update
+IF EXIST "git\launcherupdate.dat" (
+    fc .\update\launcherupdate.dat .\git\launcherupdate.dat > nul
+    IF errorlevel 1 (
+        xcopy /Y /E .\update\launcherupdate.dat .\git\launcherupdate.dat*
+    ) else (
+        del /Q update\launcherupdate.dat
+    )
+) else (
+    xcopy /Y /E .\update\launcherupdate.dat .\git\launcherupdate.dat*
+)
+xcopy /Y /E update\\. .
+del .\BepInEx\plugins\r1ft.DynamicTimeCyle.dll
+del .\BepInEx\plugins\r1ft.Headlamps.dll
+xcopy /Y /E .\user\mods\r1ft-DynamicTimeCycle\r1ft.DynamicTimeCyle.dll .\BepInEx\plugins\r1ft.DynamicTimeCyle.dll*
+xcopy /Y /E .\user\mods\.SPO\mods\Headlamps\r1ft.Headlamps.dll .\BepInEx\plugins\r1ft.Headlamps.dll*
+rmdir /Q /S update
+start "" "Aki.Launcher.exe"
+Server.exe
