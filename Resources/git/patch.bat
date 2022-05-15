7982540f65e6a0b1aadfb2c6db19b4ce3eaad390
Penn Oser <r1ft4469@users.noreply.github.com>
2022-05-15 13:30:21 -0500

    Update Launcher Files

---
+++ Resources/git/patch.bat
@@ -0,0 +1,67 @@
+@echo off
+taskkill /F /IM Aki.Launcher.exe
+taskkill /F /IM Server.exe
+IF EXIST "git\git.exe" (
+    git\git.exe -y -gm2 -InstallPath="git"
+    del git\git.exe
+    del git\git.7z.001
+)
+FOR /F "USEBACKQ" %%F IN (`powershell -NoLogo -NoProfile -Command ^(Get-Item "EscapeFromTarkov.exe"^).VersionInfo.FileVersion`) DO (SET fileVersion=%%F)
+IF "%fileVersion%"=="0.12.12.17861" (
+    call :DOWNLOAD
+    xcopy /Y /E git\\patch\\12.12.17681\\.
+    call :INSTALL
+    goto :EOF
+)
+IF "%fileVersion%"=="0.12.12.17686" (
+    call :DOWNLOAD
+    xcopy /Y /E git\\patch\\12.12.17686\\.
+    call :INSTALL
+    goto :EOF
+)
+IF "%fileVersion%"=="0.12.12.17639" (
+    call :DOWNLOAD
+    xcopy /Y /E git\\patch\\12.12.17639\\.
+    call :INSTALL
+    goto :EOF
+)
+IF "%fileVersion%"=="0.12.12.17566" (
+    call :DOWNLOAD
+    xcopy /Y /E git\\patch\\12.12.17566\\.
+    call :INSTALL
+    goto :EOF
+)
+IF "%fileVersion%"=="0.12.12.17349" (
+    echo Good Version
+    goto :EOF
+) else (
+    echo Update Tarkov or Wait for Patcher Release
+    echo Current Releases Available
+    echo ------------------
+    echo 0.12.12.17686
+    echo 0.12.12.17639
+    echo 0.12.12.17566
+    echo 0.12.12.17349 < AKI VERSION
+    pause
+    goto :EOF
+)
+
+:INSTALL
+patcher.exe
+rmdir /Q /S .\git\patch
+del patcher.exe
+start "" "cmd.exe" /c git\verify.bat
+goto :EOF
+
+:DOWNLOAD
+mkdir .\git\patch
+.\git\mingw64\bin\curl.exe -LJ -o .\git\patch\patch.dat.001 https://raw.githubusercontent.com/r1ft4469/spo-updater/update/patch.dat.001
+.\git\mingw64\bin\curl.exe -LJ -o .\git\patch\patch.dat.002 https://raw.githubusercontent.com/r1ft4469/spo-updater/update/patch.dat.002
+.\git\mingw64\bin\curl.exe -LJ -o .\git\patch\patch.dat.003 https://raw.githubusercontent.com/r1ft4469/spo-updater/update/patch.dat.003
+.\git\mingw64\bin\curl.exe -LJ -o .\git\patch\patch.dat.004 https://raw.githubusercontent.com/r1ft4469/spo-updater/update/patch.dat.004
+.\git\bin\7za.exe x -ogit\patch .\git\patch\patch.dat.001
+del .\git\patch\patch.dat.001
+del .\git\patch\patch.dat.002
+del .\git\patch\patch.dat.003
+del .\git\patch\patch.dat.004
+goto :EOF
