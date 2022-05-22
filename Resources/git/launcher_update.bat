6d9b9e2920b558bdbdfac43ffb8172aa4ee404f3
Penn Oser <r1ft4469@users.noreply.github.com>
2022-05-22 02:52:44 -0500

    Update Scripts

--- Resources/git/launcher_update.bat
+++ Resources/git/launcher_update.bat
@@ -1,14 +1,14 @@
 @echo off
-powershell write-host -fore Yellow -back DarkBlue "SPO Launcher Update ..."
-powershell write-host -fore Yellow -back DarkBlue "-----------------------------------------"
+powershell write-host -fore Cyan "SPO Launcher Update ..."
+powershell write-host -fore Cyan "-----------------------------------------"
 taskkill /F /IM Server.exe > nul 2>&1
 taskkill /F /IM Aki.Launcher.exe > nul 2>&1
-powershell write-host -fore Green "Installing new Launcher ..."
+powershell write-host -fore Yellow "Installing new Launcher ..."
 move .\git\update.dat.new .\update.exe > nul 2>&1
 update.exe -y -gm2 > nul 2>&1
 del /F /Q update.exe > nul 2>&1
-powershell write-host -fore Yellow -back DarkBlue "Finished."
-powershell write-host -fore Yellow -back DarkBlue "Starting Launcher ..."
-powershell write-host -fore Yellow -back DarkBlue "-----------------------------------------"
+powershell write-host -fore Cyan "Finished."
+powershell write-host -fore Cyan "Starting Launcher ..."
+powershell write-host -fore Cyan "-----------------------------------------"
 start "" "Aki.Launcher.exe"
 .\Server.exe
